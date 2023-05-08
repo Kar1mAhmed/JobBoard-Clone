346e4df0ca2a21d027655b7be65bd3b670e90516
Kar1mAhmed <karim77645@gmail.com>
2023-05-08 13:45:03 +0300

    added template and applied static sytle

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [  
     path('admin/', admin.site.urls),
     path('jobs/', include('job.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
