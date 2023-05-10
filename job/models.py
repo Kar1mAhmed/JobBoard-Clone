a6ae7bc2f66560e5d25b52641f34f096ad4c1776
Kar1mAhmed <karim77645@gmail.com>
2023-05-10 14:15:02 +0300

    renaming some cols

--- job/models.py
+++ job/models.py
@@ -15,8 +15,8 @@ def upload_img(instance, filename):
 class Job(models.Model):
     title = models.CharField(max_length=100)
     #location = 
-    job_time = models.CharField(max_length=15, choices=JOB_TYPES)
-    job_description = models.TextField(max_length=1000)
+    type = models.CharField(max_length=15, choices=JOB_TYPES)
+    description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
