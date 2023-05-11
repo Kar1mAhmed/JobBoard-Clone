185e49b09f30363155b083d50dfe677a0a16881f
Kar1mAhmed <karim77645@gmail.com>
2023-05-12 01:35:32 +0300

    Added signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
+from .form import SignupForm
 # Create your views here.
 
 
 def signup(request):
-    pass
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            
+            
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
+            return redirect(reverse('accounts/profile')) # go to job list page after saving
+
+    else:
+        form = SignupForm()
+    
+    context = {'form_data': form}
+    
+    return render(request, 'registration/signup.html', context)
